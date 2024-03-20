fa49c4fc2fe08d338f287ac16b1159e626bc71bd
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-03-21 01:00:28 +0800

    fix web title

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="w-full min-h-full space-y-6 p-6">
       <h1 className="text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        WebSocket Demo!
+        Live Reactions
       </h1>
       <Container />
       <EmojiSelection />
